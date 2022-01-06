3ca118461a1faf76e86551f1d391ae3c4c928827
mgpabilando <mgpabilando@sorsogonstatecollege.edu.ph>
2022-01-06 17:34:22 +0800

    dtrreport

--- app/Http/Controllers/Dtr_ReportController.php
+++ app/Http/Controllers/Dtr_ReportController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\MorningDtr;
-use App\Models\AfternoonDtr;
+use App\Models\afternoonDtr;
 use App\Models\undertimeDtr;
 use Carbon\Carbon;
 class Dtr_ReportController extends Controller
@@ -12,7 +12,7 @@ class Dtr_ReportController extends Controller
     public function index()
     {
         $morningrecord = MorningDtr::get();
-        $afternoonrecord = AfternoonDtr::get();
+        $afternoonrecord = afternoonDtr::get();
         $undertimerecord = undertimeDtr::get();
         $todayMonth = Carbon::now()->format('F, Y', 'Philippines');
         return view('navigation_links.reports.daily_timerec', compact('todayMonth', 'morningrecord', 'afternoonrecord', 'undertimerecord'));
