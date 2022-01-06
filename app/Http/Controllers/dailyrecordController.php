8b9aeb438360a1a9033d2dd3f3a2f332dca1bea2
mgpabilando <mgpabilando@sorsogonstatecollege.edu.ph>
2022-01-06 16:49:06 +0800

    dtr

--- app/Http/Controllers/dailyrecordController.php
+++ app/Http/Controllers/dailyrecordController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\AfternoonDtr;
+use App\Models\afternoonDtr;
 use App\Models\MorningDtr;
 use App\Models\undertimeDtr;
 use Illuminate\Support\Facades\Auth;
@@ -11,7 +11,7 @@ use Carbon\Carbon;
 class dailyrecordController extends Controller
 {
     public function records(){
-        $afternoon = AfternoonDtr::get();
+        $afternoon = afternoonDtr::get();
         $morning = MorningDtr::get();
         $undertime = undertimeDtr::get();
 
