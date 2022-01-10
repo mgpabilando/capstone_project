8b4e5490adaa4f190454539916ee0c6048f2260d
johnkenneth24 <kengerero@yahoo.com>
2022-01-10 14:38:22 +0800

    Merge branch 'master' of https://github.com/mgpabilando/capstone_project

--- resources/views/auth/forgetPasswordLink.blade.php
+++ resources/views/auth/forgetPasswordLink.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.loginandregister')
 
-@section('content') 
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -13,8 +13,8 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                            <div class="col-md-6">
+                            <div class="col-12">
+                                <label for="email_address" class="form-label">E-mail Address</label>
                                 <input type="text" id="email_address" class="form-control" name="email" required autofocus>
                                 @if ($errors->has('email'))
                                     <span class="text-danger">{{ $errors->first('email') }}</span>
@@ -22,33 +22,31 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                        <div class="form-group mt-2 row">
                             <div class="col-md-6">
-                                <input type="password" id="password" class="form-control" name="password" required autofocus>
+                                <label for="password" class="form-label">New Password</label>
+                                <input type="password" id="password" class="form-control" name="password" required autofocus title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters">
                                 @if ($errors->has('password'))
                                     <span class="text-danger">{{ $errors->first('password') }}</span>
                                 @endif
-                            </div>
-                        </div>
+                              </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
-                            <div class="col-md-6">
+                              <div class="col-md-6">
+                                <label for="password-confirm" class="form-label">Confirm Password</label>
                                 <input type="password" id="password-confirm" class="form-control" name="password_confirmation" required autofocus>
                                 @if ($errors->has('password_confirmation'))
                                     <span class="text-danger">{{ $errors->first('password_confirmation') }}</span>
                                 @endif
-                            </div>
+                              </div>
                         </div>
-
-                        <div class="col-md-6 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
+                        <hr>
+                        <div class="row">
+                            <button type="submit" class="btn col-md-4 btn-primary">
                                 Reset Password
                             </button>
                         </div>
                     </form>
-                      
+
                 </div>
             </div>
         </div>
